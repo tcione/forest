bdaaf12672ea8f996eb58234f5e25f4be1bf3b69
Tales Cione <talesj@gmail.com>
2025-09-02 22:48:38 +0200

    feat: naive Clone implementation

--- src/commands/clone.rs
+++ src/commands/clone.rs
@@ -1,3 +1,23 @@
-pub fn run(repository_address: String) {
-    println!("[NOT IMPLEMENTED] Cloning {repository_address}");
+use std::path::PathBuf;
+
+pub fn run(roots_dir: &PathBuf, repository_address: String) -> Result<(), Box<dyn std::error::Error>> {
+    let gitless_repo_address = repository_address.replace(".git", "");
+    let repo_name = gitless_repo_address.split('/').last().unwrap();
+    let repo_dir = roots_dir.join(repo_name);
+    let output = std::process::Command::new("git")
+        .args([
+            "clone",
+            "--bare",
+            &repository_address
+        ])
+        .arg(&repo_dir)
+        .output()?;
+
+    if !output.status.success() {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        return Err(format!("Git clone failed: {}", stderr).into());
+    }
+
+    println!("{} cloned into {}", repo_name, repo_dir.display());
+    Ok(())
 }
